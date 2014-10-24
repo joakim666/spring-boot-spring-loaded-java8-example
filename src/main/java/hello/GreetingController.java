a9793ec1c3be398fae8293b3b163834a063136b9
joakim666 <joakim@pjek.se>
2014-10-24 13:56:47 +0200

    Logging using logback.

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,5 +1,7 @@
 package hello;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,8 @@ import java.util.function.Function;
 @Controller
 public class GreetingController {
 
+    private Log log = LogFactory.getLog(GreetingController.class);
+
     List<String> persons = Arrays.asList("Walter White", "Heisenberg", "Jesse Pinkman", "Saul Goodman", "Gustavo Fringe");
 
     Function<List<String>, String> random = (List<String> list) -> list.get(new Random().nextInt(list.size() - 1));
@@ -21,6 +25,7 @@ public class GreetingController {
     @Layout("layouts/logged_in")
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+        log.debug("In greeting()");
         model.addAttribute("name", name);
         return "greeting";
     }
