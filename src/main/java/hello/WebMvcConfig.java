8c3cf5ab242822f21be39b735748839325794409
joakim666 <joakim@pjek.se>
2014-10-29 16:19:42 +0100

    Extend from WebMvcConfigurerAdapter instead so that @AuthenticationPrincipal will work.

--- src/main/java/hello/WebMvcConfig.java
+++ src/main/java/hello/WebMvcConfig.java
@@ -2,13 +2,14 @@ package hello;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.spring.support.ThymeleafLayoutInterceptor;
 
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurationSupport {
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
     @Override
-    protected void addInterceptors(InterceptorRegistry registry) {
+    public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new ThymeleafLayoutInterceptor());
     }
 }
